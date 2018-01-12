c21e75a2759eeaed6df6f6e2335bc7536a6fdee1
Aaron Ross <ashmortar@gmail.com>
2018-01-12 12:03:59 -0800

    add routes to App.java and content to some templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 
+import models.Team;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -14,25 +15,75 @@ public class App{
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        //get: landing page
+        //get: landing page per guidelines will show all teams and descriptions
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
+            ArrayList<Team> teams = Team.getAllTeams();
+            model.put("teams", teams);
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        //get: show all teams
+        //get: show about
+        get("/about", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "about.hbs");
+        }, new HandlebarsTemplateEngine());
 
         //get: show form to add a new team
+        get("/teams/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "team-form.hbs");
+        }, new HandlebarsTemplateEngine());
 
         //post: process new team form
+        post("/team/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String newTeamName = request.queryParams("name");
+            String newTeamDescription = request.queryParams("descriptoin");
+            Team newTeam = new Team(newTeamName, newTeamDescription);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
 
         //get: show a single team and form to add members
+        get("/teams/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfTeamToFind = Integer.parseInt(request.params("id"));
+            Team foundTeam = Team.findTeamById(idOfTeamToFind);
+            model.put("team", foundTeam);
+            return new ModelAndView(model, "team-detail.hbs");
+        }, new HandlebarsTemplateEngine());
 
         //post: process form to add members
+        post("/teams/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String newMember = request.queryParams("member-name");
+            int idOfTeamToAddMembersTo = Integer.parseInt(request.params("id"));
+            Team teamToAddMemberTo = Team.findTeamById(idOfTeamToAddMembersTo);
+            teamToAddMemberTo.addMember(newMember);
+            return new ModelAndView(model, "member-success.hbs");
+        }, new HandlebarsTemplateEngine());
 
         //get: show form to update team name/description
+        get("/teams/:id/update", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            int idOfTeamToUpdate = Integer.parseInt(request.params("id"));
+            Team updateTeam = Team.findTeamById(idOfTeamToUpdate);
+            model.put("updateTeam", updateTeam);
+            return new ModelAndView(model, "team-form.hbs");
+        }, new HandlebarsTemplateEngine());
 
         //post: process update form
+        post("/teams/:id/update", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String updatedName = request.queryParams("name");
+            String updatedDescription = request.queryParams("description");
+            int idOfTeamToUpdate = Integer.parseInt(request.params("id"));
+            Team updateTeam = Team.findTeamById(idOfTeamToUpdate);
+            updateTeam.updateName(updatedName);
+            updateTeam.updateDescription((updatedDescription));
+            return new ModelAndView(model,"success.hbs");
+        }, new HandlebarsTemplateEngine());
+
 
 
 
